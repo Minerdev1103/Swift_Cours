73a1d3c4cd4b2faa691532be8adfbaff561836da
Minerdev1103 <121298556+Minerdev1103@users.noreply.github.com>
2024-06-22 22:02:41 +0700

    update

---
+++ MyFirstApp/MyFirstApp/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by Earth Minerdev . on 22/6/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var name:String = "วันนี้ กินอะไรดี"
+    @State var menu:Int = 0
+    @State var count:Int = 0
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text(name)
+                .font(.title2)
+                .foregroundColor(.purple)
+                .frame(width: 300, height: 100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center)
+    
+            Button("Button") { 
+                
+                
+                
+                self.menu = Int.random(in: 1...10)
+                let menulist = [กระเพราไก่,ข้าวผัด,สุกกี้,ข้าวคลุกกะปิ]
+                
+                self.name = menulist[self.menu]
+                
+                self.count  += 1
+                
+            }
+            
+            
+            
+
+            Text("สุ่มเลย "+String(count)+" ครั้ง")
+                }
+        
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyFirstApp/MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,16 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp//
+//  Created by Earth Minerdev . on 22/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
